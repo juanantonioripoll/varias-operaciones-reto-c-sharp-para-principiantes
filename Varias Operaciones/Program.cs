6f76508846aa904416bb63f7f4ad01bb89a818d9
anonymous <anonymous@ANONYMOUSDEV>
2024-01-03 12:40:53 +0100

    Agregar archivos de proyecto.

---
+++ Varias Operaciones/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varias_Operaciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un número");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduce otro número");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} + {1} = {2}", number1, number2, number1 + number2);
+            Console.WriteLine("{0} - {1} = {2}", number1, number2, number1 - number2);
+            Console.WriteLine("{0} X {1} = {2}", number1, number2, number1 * number2);
+            Console.WriteLine("{0} / {1} = {2}", number1, number2, number1 / number2);
+            Console.WriteLine("{0} mod {1} = {2}", number1, number2, number1 % number2);
+            Console.ReadLine();
+        }
+    }
+}
